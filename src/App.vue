0fbd0c04dbb60d55e17aa9a316dacc396ac8b053
Dawid Majchrzak <programista.dawid.majchrzak@gmail.com>
2022-10-24 12:04:20 +0200

    First Commit - already axios fetch

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script >
+import axios from 'axios';
+
+export default {
+  name: "Beer_App",
+  data() {
+    return {
+      beers: []
+    }
+  },
+  async created() {
+    try {
+      const res = await axios.get('https://api.punkapi.com/v2/beers');
+      this.beers = res.data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  // methods: {
+  //   async getData() {
+  //     await axios.get('https://api.punkapi.com/v2/beers')
+  //       .then(function (response) {
+  //         console.log(response)
+  //         this.beers = response.data
+  //       })
+  //       .catch(function (error) {
+  //         // handle error
+  //         console.log(error);
+  //       })
+  //       .then(function () {
+  //         // always executed
+  //       })
+  //   }
+  // }
+}
+
+</script>
+
+<template>
+  <p>siema</p>
+  <ul>
+    <li v-for="beer in beers">
+      <p>{{beer.name}}</p>
+      <img src={{beer.image_url}} alt="">
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
